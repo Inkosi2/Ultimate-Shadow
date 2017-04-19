a6bdf4fed5abd15014eab938ed00b8fbec324a35
Unknown <oscarmasferrerrubio@enti.cat>
2017-04-19 10:17:09 +0200

    Project creation

---
+++ Proyecto sombra/Assets/EaglosBehaving.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EaglosBehaving : MonoBehaviour {
+
+    float EaglosSpeed;
+    double BossPlayerAngle;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        EaglosSpeed = 1.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BossPlayerAngle = Math.Atan((transform.position.y-player.transform.position.y)/(transform.position.x - player.transform.position.x));
+        //GetComponent<Rigidbody2D>().velocity = new Vector2 ()
+	}
+}
