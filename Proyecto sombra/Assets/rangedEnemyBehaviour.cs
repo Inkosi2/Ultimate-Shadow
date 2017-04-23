274251b240892680d47524ed3f2807e1445e3b86
Unknown <albertnievesm@gmail.com>
2017-04-23 03:22:51 +0200

    Añadido arquero, pendiente de arreglar

--- Proyecto sombra/Assets/rangedEnemyBehaviour.cs
+++ Proyecto sombra/Assets/rangedEnemyBehaviour.cs
@@ -4,37 +4,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class meleeEnemyBehaviour : MonoBehaviour {
+public class rangedEnemyBehaviour : MonoBehaviour
+{
     public double Speed, distX, distY, moduloDist, uniX, uniY;
     //regresar a posición inicial
     public double distX0, distY0, moduloDist0, uniX0, uniY0, Xinicial, Yinicial;
     public bool Attacking;
     public int auxAttack;
-    public bool attackDone, aggro, vulnerability;
+    public bool attackDone, aggro, estatico;
     //auxiliares
     public int aux;
 
 
 
     public float time;
-   
+
     public GameObject player;
 
     // Use this for initialization
-    void Start () {
-        Speed = 2;
+    void Start()
+    {
+        Speed = 6;
         Attacking = false;
         attackDone = true;
-        Xinicial=transform.position.x;
-        Yinicial=transform.position.y;
+        Xinicial = transform.position.x;
+        Yinicial = transform.position.y;
         //Inicializar valores de regreso a su posición inicial.
         moduloDist0 = 1;
     }
     //Función para que persiga al jugador.
-    void Chase()
+    void Flee()
     {
         //Movimiento de Eaglos: Desplazarse hacia el jugador
-        GetComponent<Rigidbody2D>().velocity = new Vector2(System.Convert.ToSingle(Speed * -uniX), System.Convert.ToSingle(Speed * -uniY));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(System.Convert.ToSingle(Speed * uniX), System.Convert.ToSingle(Speed * uniY));
     }
 
     //Función para hacer el ataque básico.
@@ -42,7 +44,7 @@ public class meleeEnemyBehaviour : MonoBehaviour {
     {
 
         //Asegurarsse que el jugador está cerca para iniciar el ataque.
-        if (moduloDist < 2)
+        if (moduloDist < 10 && moduloDist>5)
         {
             //Comprovar si ha empzado a atacar o esta en proceso.
             if (attackDone == true)
@@ -50,9 +52,9 @@ public class meleeEnemyBehaviour : MonoBehaviour {
                 //Determinar el timer para la duración de las fases del ataque.
                 time = 0;
                 //Indicar que el ataque se acaba de iniciar.
-                attackDone = false;
-                vulnerability = true;
+                attackDone = false;               
             }
+        
         }
         //Continuar el ataque ya iniciado inclusi si el jugador no está cerca.
         if (attackDone == false)
@@ -82,7 +84,7 @@ public class meleeEnemyBehaviour : MonoBehaviour {
             {
                 //(Provisional) Marcar la fase recomponerse tras atacar. A la espera de sprite.
                 GetComponent<SpriteRenderer>().color = Color.green;
-                
+
                 auxAttack++;
             }
             //Permitir otro bucle cuando el tiempo entre fases no ha pasado.
@@ -96,7 +98,7 @@ public class meleeEnemyBehaviour : MonoBehaviour {
                 time = 0;
                 auxAttack++;
                 attackDone = true;
-                vulnerability = false;
+               
             }
             //Permitir otro bucle cuando el tiempo entre fases no ha pasado.
             else { Attacking = false; }
@@ -106,14 +108,12 @@ public class meleeEnemyBehaviour : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
 
         //Contador de tiempo
-        if (aggro)
-        {
-            time += Time.deltaTime;
-        }  
+      
+        time += Time.deltaTime;
 
         //Movimiento de el enemigo: Obtener vector hacia el jugador.
         distX = transform.position.x - player.transform.position.x;
@@ -124,37 +124,6 @@ public class meleeEnemyBehaviour : MonoBehaviour {
         uniX = distX / moduloDist;
         uniY = distY / moduloDist;
 
-        //El enemigo pilla aggro del jugador a cierta distancia.
-        if(Attacking == false && attackDone == true && moduloDist < 5)
-        {
-            aggro = true;
-        }
-
-        //Perseguir el jugador si tiene aggro.
-        if (aggro )
-        {
-            
-            Chase();
-        }
-        //Deja de perseguirlo a cierto punto.
-        else
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
-
-        //Pierde el aggro si el jugador está lejos suficiente tiempo
-        if (moduloDist > 7 && aggro==true && time > 3 && Attacking == false)
-        {
-            aggro = false;
-        }
- 
-
-        //Atacar al llegar a cierta distancia al jugador
-        if (Attacking == false )
-        {
-            Attack();
-        }
-
         //Calcular datos para la ruta de vuelta a su posición inical.
         distX0 = transform.position.x - Xinicial;
         distY0 = transform.position.y - Yinicial;
@@ -164,13 +133,46 @@ public class meleeEnemyBehaviour : MonoBehaviour {
         uniX0 = distX0 / moduloDist0;
         uniY0 = distY0 / moduloDist0;
 
+        //El enemigo pilla aggro del jugador a cierta distancia.
+        if (moduloDist < 10 && moduloDist > 5)
+        {
+            //aggro = true;
+            //Attack();
+        }
+
+        else if (moduloDist<=5)
+        {
+            //aggro = false;
+            //Flee();
+        }
+
+       else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(System.Convert.ToSingle(Speed * -uniX0), System.Convert.ToSingle(Speed * -uniY0));
+        }
+      
+    /*
+        //Pierde el aggro si el jugador está lejos suficiente tiempo
+        if (moduloDist > 10 && aggro == true && Attacking == false)
+        {
+            aggro = false;
+        }
+
+
+        //Atacar al llegar a cierta distancia al jugador
+        if (Attacking == false)
+        {
+            Attack();
+        }
+
+       
+
         //El enemigo regresa a su puesto tras perder de vista al jugador.
-        if (!aggro && !Attacking && moduloDist0 > 0.1 )
+        if (!aggro && !Attacking && moduloDist0 > 0.1)
         {
             time = 0;
             GetComponent<Rigidbody2D>().velocity = new Vector2(System.Convert.ToSingle(Speed * -uniX0), System.Convert.ToSingle(Speed * -uniY0));
         }
-
+        */
     }
 }
-
