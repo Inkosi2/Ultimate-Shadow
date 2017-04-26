675e0917a64eb526342902e296f46aa0ae2b86ed
Maria López <marialopezvivas@enti.cat>
2017-04-26 13:58:34 +0200

    Arreglado muro plataforma

---
+++ Proyecto sombra/Assets/MuroPlataforma.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuroPlataforma : MonoBehaviour {
+    public Collider2D muro;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+    void OnTriggerEnter2D (Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Platform")
+        {
+            coll.isTrigger = true;
+            muro.isTrigger = true;
+        }
+        else
+        {
+            coll.isTrigger = false;
+            muro.isTrigger = false;
+        }
+    }
+}
