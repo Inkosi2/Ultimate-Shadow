07f3192d3a6314d800447916530eab414e8d9484
Maria López <marialopezvivas@enti.cat>
2017-05-12 13:59:22 +0200

    Sombra del bosque

---
+++ Proyecto sombra/Assets/Scripts/Enemies/SombraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SombraBehaviour : MonoBehaviour {
+
+    public double EaglosSpeed, distX, distY, moduloDist, uniX, uniY; //Detectar al jugador.
+    public int fase, HP, SombraSpeed;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        SombraSpeed = 6;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Vectos hacia el jugador.
+        distX = transform.position.x - player.transform.position.x;
+        distY = transform.position.y - player.transform.position.y;
+        
+        moduloDist = Math.Sqrt(Math.Pow(distX, 2) + Math.Pow(distY, 2));
+        
+        uniX = distX / moduloDist;
+        uniY = distY / moduloDist;
+
+    }
+}
