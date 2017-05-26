d5f5c3eca748872b8fccfdabbfa1730e06fb7972
Maria López <marialopezvivas@enti.cat>
2017-05-26 12:41:20 +0200

    Box code

---
+++ Proyecto sombra/Assets/THB.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class THB : MonoBehaviour
+{
+
+    public GameObject lado;
+    public GameObject box;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Jugador")
+        {
+            if (lado.name == "THB")
+            {
+                box.GetComponent<Rigidbody2D> ().velocity = new Vector2(0, -1);
+            }
+            if (lado.name == "LHB")
+            {
+                box.GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
+            }
+            if (lado.name == "RHB")
+            {
+                box.GetComponent<Rigidbody2D>().velocity = new Vector2(-1, 0);
+            }
+            if (lado.name == "DHB")
+            {
+                box.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+            }
+        }
+    }
+}
