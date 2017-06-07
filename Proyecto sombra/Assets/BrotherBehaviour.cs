2e9b6a272d07183d860d1bf1b09e938d24200683
Albert <albertnievesm@gmail.com>
2017-06-07 08:42:31 +0200

    Sombra maestra

--- Proyecto sombra/Assets/BrotherBehaviour.cs
+++ Proyecto sombra/Assets/BrotherBehaviour.cs
@@ -1,39 +1,120 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+
 
 public class BrotherBehaviour : MonoBehaviour {
 
+    public double distX, distY, moduloDist, uniX, uniY, mouthX, mouthY;
     float dirX, dirY;
-    int fase;
+    public int fase, proyectilSpd;
+    public GameObject player, RightEye, LeftEye;
+    public GameObject proyectil;
+    
 	// Use this for initialization
 	void Start () {
+        proyectilSpd = 5;
+       
+        mouthX = 0;
+        mouthY = 16;
+        fase = 1;
         dirX = 10;
         dirY = 10;
 	}
 
     void OnCollisionEnter2D(Collision2D bounce)
     {
-        if (bounce.gameObject.tag == "sideWall")
+        if (bounce.gameObject.tag == "sideWall" && fase == 1)
         {
             dirX = -dirX;
+            burst();
         }
 
-        if (bounce.gameObject.tag == "horitzontalWall")
+        if (bounce.gameObject.tag == "horitzontalWall" && fase == 1)
         {
             dirY = -dirY;
+            burst();
         }
     }
 
     // Update is called once per frame
     void Update () {
+        //Obtener vector hacia el jugador.
+        distX = mouthX - transform.position.x;
+        distY = mouthY - transform.position.y;
+
+        moduloDist = Math.Sqrt(Math.Pow(distX, 2) + Math.Pow(distY, 2));
+
+        uniX = distX / moduloDist;
+        uniY = distY / moduloDist;
+
+        if(RightEye.GetComponent<ShadowEyes>().hurt == true && LeftEye.GetComponent<ShadowEyes>().hurt == true)
+            
+        
+
         if (fase == 1)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(dirX, dirY);
         }
+        
         else
         {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            if (moduloDist > 0.1) GetComponent<Rigidbody2D>().velocity = new Vector2((float)uniX * 10, (float)uniY * 10);
 
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                darkWave();
+            }
         }
+    }
+
+    void burst ()
+    {
+        proyectil= (GameObject)Instantiate(proyectil);        
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(-proyectilSpd, 0);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(proyectilSpd, 0);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(0, proyectilSpd);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -proyectilSpd);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(proyectilSpd, proyectilSpd);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(-proyectilSpd, proyectilSpd);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(proyectilSpd, -proyectilSpd);
+
+        proyectil = (GameObject)Instantiate(proyectil);
+        proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+        proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(-proyectilSpd, -proyectilSpd);   
+
+    }
+    void darkWave ()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            float randomX1 = UnityEngine.Random.Range(0.7f, -0.7f);
+            proyectil = (GameObject)Instantiate(proyectil);
+            proyectil.transform.position = new Vector2(transform.position.x, transform.position.y);
+            proyectil.GetComponent<Rigidbody2D>().velocity = new Vector2(randomX1 * 10, -10);
+        }        
     }
 }
---
+++ Proyecto sombra/Assets/ShadowEyes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowEyes : MonoBehaviour {
+   public  bool hurt;
+	// Use this for initialization
+	void Start ()
+    {
+        hurt = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       if (hurt)
+            GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Arrow")       
+            hurt = true;
+        
+    }
+}
